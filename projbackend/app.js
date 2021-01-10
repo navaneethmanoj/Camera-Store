699508fe3099e1c08464183017e9ce35fd441840
navaneethmanoj <36037989+navaneethmanoj@users.noreply.github.com>
2021-01-11 01:41:39 +0530

    Add app.js and package.json

---
+++ projbackend/app.js
@@ -0,0 +1,50 @@
+require("dotenv").config();
+
+const mongoose = require("mongoose");
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+
+const authRoutes = require("./routes/auth.js");
+const userRoutes = require("./routes/user");
+const categoryRoutes = require("./routes/category");
+const productRoutes = require("./routes/product");
+const orderRoutes = require("./routes/order");
+const paymentBRoutes = require("./routes/paymentB");
+
+//Database connection
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  })
+  .then(() => {
+    console.log("DB CONNECTED");
+  })
+  .catch(() => {
+    console.log("DB CONNECTION ERROR");
+  });
+
+//Middlewares
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(cors());
+
+//Routes
+app.use("/api", authRoutes);
+app.use("/api", userRoutes);
+app.use("/api", categoryRoutes);
+app.use("/api", productRoutes);
+app.use("/api", orderRoutes);
+app.use("/api", paymentBRoutes);
+
+//PORT
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+  console.log(`app is running at ${port}`);
+});
